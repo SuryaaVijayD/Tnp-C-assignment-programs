71138761fdc5becae108180ffb68fd942444c5fe
Suryaa <119103117+SuryaaVijay@users.noreply.github.com>
2024-01-28 08:42:12 +0530

    Update and rename prg-3.txt to prg-3.c

---
+++ Tnp/day2/prg-3.c
@@ -0,0 +1,18 @@
+// To make string in to palindrome
+
+
+#include<stdio.h>
+int main() {
+    char str[100];
+    int l = 0, i;
+    printf("Enter a string: ");
+    gets(str);
+    while(str[l] != '\0') l++;
+    for(i = l - 1; i >= 0 ; i--) str[l++] = str[i];
+    str[l] = '\0';
+    printf("The palindrome string is: %s", str);
+    return 0;
+ }
+
+// I/P : palindrome
+// O/P : palindromeemordnilap
